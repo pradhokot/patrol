d7ecd24b6c86283c9b6864e7e1083262936d2984
pradhokot <yoggi60@gmail.com>
2025-09-03 14:07:16 +0700

    update9

--- template/backend/layout/account.js
+++ template/backend/layout/account.js
@@ -17,7 +17,7 @@ document.writeln(`
          </li>
          <li><div class="dropdown-divider"></div></li>
          <li>
-            <a href="#" class="dropdown-item d-flex align-items-center gap-2 text-capitalize text-reset">
+            <a href="signin.html" class="dropdown-item d-flex align-items-center gap-2 text-capitalize text-reset">
                <i class="link-icon" aria-hidden="true">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill d-flex" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0"/><path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/></svg>
                </i>
